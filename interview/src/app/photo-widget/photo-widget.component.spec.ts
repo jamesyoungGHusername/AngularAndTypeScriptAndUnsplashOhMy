747ec69d9cf3d65d6c0ee279b3544572351bd1aa
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2023-02-02 11:37:32 -0700

    generated files for services and photo widget component

---
+++ interview/src/app/photo-widget/photo-widget.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhotoWidgetComponent } from './photo-widget.component';
+
+describe('PhotoWidgetComponent', () => {
+  let component: PhotoWidgetComponent;
+  let fixture: ComponentFixture<PhotoWidgetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PhotoWidgetComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PhotoWidgetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ interview/src/app/photo-widget/photo-widget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-widget',
+  templateUrl: './photo-widget.component.html',
+  styleUrls: ['./photo-widget.component.css']
+})
+export class PhotoWidgetComponent {
+
+}
